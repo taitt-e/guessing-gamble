ff6c35aa3fd542fa3672fc09a2daf0607aa56c2e
taitt-e <taittestes20@outlook.com>
2024-04-09 00:48:41 -0500

    got basis for Dynamic Programming made, has logic errors.

--- dynamicProg.py
+++ dynamicProg.py
@@ -1,18 +1,63 @@
 import argparse
 import time
 
-def dynamicProgrammingFind(minRange, maxRange, target, jackpot):
-    print("minRange:" + str(minRange))
-    print("minRange:" + str(maxRange))
-    print("minRange:" + str(target))
-    print("minRange:" + str(jackpot))
+def dynamicProgrammingFind(minRange, maxRange, target):
+    print("Finding: " + str(target))
+    tempInt = minRange
+    breakBool = True
+    operator = 2
+    guess_count = 0
+    ##Find a smaller subrange to begin iterating on via dynamic division
+    while (breakBool):
+        if(tempInt == target):
+            return tempInt,guess_count
+        guess_count+=1
+        if(target > (maxRange//operator)):
+            operator-=1
+            tempInt = maxRange//operator
+            break
+        else:
+            operator+=1
+    if(tempInt == target):
+        return tempInt,guess_count
+    
+    ##Further close the gap via dynamic multiplication with loops
+    ##Not properly iterating here, I believe its a logic error - TE
+    operator = 1
+    while(breakBool):
+        for x in range(operator):
+            guess_count+=1
+            if(tempInt+(operator*x) > target):
+                tempInt = tempInt+(operator*(x-1))
+                breakBool = False
+        operator+=1
+    if(tempInt == target):
+        return tempInt,guess_count
+    
+    ##Close the gap even more with dynamic addition
+    ##Not properly iterating here, I believe its a logic error - TE
+    operator = 1
+    breakBool = True
+    while(breakBool):
+            for x in range(operator):
+                guess_count+=1
+                if(tempInt+(operator+x) > target):
+                    tempInt += (operator+x)
+                    breakBool = False
+                    break
 
-    ##Base setup is done, will begin making the algorithm tomorrow - TE
-    return 0, 0
+            operator+=1
+    
+    ##Finally close the gap with subtraction
+    while(tempInt != target):
+        guess_count+=1
+        tempInt-=1
+    
+    return tempInt, guess_count
 
 def main(min_range, max_range, target, jackpot):
     start_time = time.perf_counter()  # Start time in microseconds
-    guess, guess_count = dynamicProgrammingFind(min_range, max_range, target, jackpot)
+    guess, guess_count = dynamicProgrammingFind(min_range, max_range, target)
     end_time = time.perf_counter()  # End time in microseconds
     elapsed_time = (end_time - start_time) * 1_000_000  # Time taken to find the correct value
 
